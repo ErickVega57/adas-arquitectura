40bb8ae672bd9f133a05eff7310f86d77b756c65
ErickVega57 <A21203354@alumnos.uady.mx>
2026-09-06 01:03:44 -0600

    ADA4(mazegame): Cambio en la organización del código

---
+++ ADA4/mazegame/src/model/TimeKeeper.java
@@ -0,0 +1,26 @@
+package src.model;
+
+public class TimeKeeper {
+    //a class to keep track of the total seconds and minuntes the player has used to get to a leve
+    int minutes=0; 
+    int seconds=0; 
+
+    public void TimeKeeper(int min, int sec) { 
+	    if(sec + seconds <=60) {
+	        minutes+=min;
+	        seconds=sec+seconds;
+        } else {
+	        minutes+=min;
+	        minutes+=1*((sec+seconds)/60);
+	        seconds=(sec+seconds)%60;
+	    }
+    }//end TimeKeeper
+	
+	public int getMinutes() {
+	    return minutes;
+	}
+	
+	public int getSeconds() {
+	    return seconds;
+    }	
+}//end class
