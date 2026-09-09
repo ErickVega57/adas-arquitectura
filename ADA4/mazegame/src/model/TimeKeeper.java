0aa6a95834301ddd6f62f215020aad1806cd38c6
NotRussell <russellcanto123@gmail.com>
2026-09-09 17:39:52 -0600

    Refactorizar codigo
    Se implementó el patron Singleton en la clase  TimeKeeper para mejor control de instancias y asi ayuda a mejorar la encapsulación

--- ADA4/mazegame/src/model/TimeKeeper.java
+++ ADA4/mazegame/src/model/TimeKeeper.java
@@ -1,26 +1,43 @@
 package src.model;
 
 public class TimeKeeper {
-    //a class to keep track of the total seconds and minuntes the player has used to get to a leve
-    int minutes=0; 
-    int seconds=0; 
 
-    public void TimeKeeper(int min, int sec) { 
-	    if(sec + seconds <=60) {
-	        minutes+=min;
-	        seconds=sec+seconds;
-        } else {
-	        minutes+=min;
-	        minutes+=1*((sec+seconds)/60);
-	        seconds=(sec+seconds)%60;
-	    }
-    }//end TimeKeeper
-	
-	public int getMinutes() {
-	    return minutes;
+	private static TimeKeeper instance;
+
+	private int minutes = 0;
+	private int seconds = 0;
+
+	private TimeKeeper() {
 	}
-	
+
+	public static TimeKeeper getInstance() {
+		if (instance == null) {
+			instance = new TimeKeeper();
+		}
+		return instance;
+	}
+
+	public void TimeKeeper(int min, int sec) {
+		if (sec + seconds <= 60) {
+			minutes += min;
+			seconds = sec + seconds;
+		} else {
+			minutes += min;
+			minutes += 1 * ((sec + seconds) / 60);
+			seconds = (sec + seconds) % 60;
+		}
+	}
+
+	public int getMinutes() {
+		return minutes;
+	}
+
 	public int getSeconds() {
-	    return seconds;
-    }	
-}//end class
+		return seconds;
+	}
+
+	public void resetTime() {
+		this.minutes = 0;
+		this.seconds = 0;
+	}
+}
