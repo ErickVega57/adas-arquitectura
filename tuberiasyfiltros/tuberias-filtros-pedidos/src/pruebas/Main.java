65845b7d9568e10c8d226e69db498b210bfb44e6
ErickVega57 <A21203354@alumnos.uady.mx>
2026-09-17 19:57:08 -0600

    TuberiasyFiltros: Clase tuberia

--- tuberiasyfiltros/tuberias-filtros-pedidos/src/pruebas/Main.java
+++ tuberiasyfiltros/tuberias-filtros-pedidos/src/pruebas/Main.java
@@ -1,8 +1,24 @@
 package pruebas;
 
+import entidades.Pedido;
+import entidades.Producto;
+import filtros.AplicarDescuento;
+import filtros.CalcularSubtotal;
+import filtros.ComprobarDisponibilidad;
+import filtros.ValidarDatos;
+import java.util.List;
+import tuberias.Tuberia;
+
 public class Main {
     public static void main(String[] args) {
+        Pedido pedido = new Pedido("Erick", 
+        List.of(new Producto("lapiz", 45.5, 3,10), 
+        new Producto("Cepillo", 20, 1, 14)));
 
+
+        Tuberia tuberia = new Tuberia(List.of(new ComprobarDisponibilidad(), new CalcularSubtotal(), new AplicarDescuento(), new ValidarDatos()));
+        tuberia.ejecutar(pedido);
+        
         return;
     }
 }
--- tuberiasyfiltros/tuberias-filtros-pedidos/src/tuberias/Tuberia.java
+++ tuberiasyfiltros/tuberias-filtros-pedidos/src/tuberias/Tuberia.java
@@ -1,5 +1,22 @@
 package tuberias;
 
+import entidades.Pedido;
+import filtros.Filtro;
+import java.util.List;
+
 public class Tuberia {
 
+    private List<Filtro> filtros;
+
+    public Tuberia(List<Filtro> filtros) {
+        this.filtros = filtros;
+    }
+
+    public Pedido ejecutar(Pedido pedido){
+        for(Filtro filtro: filtros){
+            filtro.procesar(pedido);
+        }
+        return pedido;
+    }
+    
 }
